a8718c9d18a131906a24f8dfbc0817831a362112
Leonardo Price <leonardo.price@gmail.com>
2020-06-16 01:36:47 -0400

    top down example scene

---
+++ Assets/Scripts/ysort.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ysort : MonoBehaviour
+{
+    public float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+private void LateUpdate()
+{
+    GetComponent<Renderer>().sortingOrder = (int)(500f - (offset * 10f + transform.position.y * 10f));
+}
+}
