1ab65b1ac283103774c0441515e78b59509968a1
Jen <bourkj@rpi.edu>
2020-06-17 15:01:49 -0400

    BeginInventoryDrop

---
+++ Assets/Scripts/UI/InventoryDrop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryDrop : MonoBehaviour
+{
+    [SerializeField] private Animator _inventoryDrop;
+    void OnMouseOver(){
+        _inventoryDrop.SetBool("hovering", true);
+        print("hewwo");
+    }
+    void OnMouseExit(){
+        _inventoryDrop.SetBool("hovering", false);
+    }
+}
