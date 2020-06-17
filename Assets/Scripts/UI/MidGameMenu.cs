a0af3b7b621caade53b1de393bdd0add8ac33675
Jen <bourkj@rpi.edu>
2020-06-17 14:14:47 -0400

    Make menu open and close

---
+++ Assets/Scripts/UI/MidGameMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidGameMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _menu;
+    [SerializeField] private GameObject _button;
+
+    public void OnClick()
+    {
+        _menu.SetActive(true);
+        _button.SetActive(false);
+    }
+
+    public void OnClickBack()
+    {
+        _menu.SetActive(false);
+        _button.SetActive(true);
+    }
+}
