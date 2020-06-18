4cb2d6d2f34fffacabd8b01ed0de70a35d84dbaf
Zaire Wilson <Zaire.R.Wilson@gmail.com>
2020-06-17 22:16:21 -0400

    NPC prefab and more dialogue refinement

--- Assets/Scripts/Physics/Player.cs
+++ Assets/Scripts/Physics/Player.cs
@@ -1,47 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class Player : MonoBehaviour
 {
-
     [SerializeField]
     private float move_speed;
     [SerializeField]
-    private float diag_speed; 
+    private float diag_speed;
     private Vector3 input;
 
-    public GameObject DiaRunner; 
+    private bool inside_npc_radius = false;
+    public string current_npc_startNode = "";
+
 
     public GameObject interaction_bubble;
 
-    public bool visited = false; 
 
-    private CharacterController char_cont; 
+    private CharacterController char_cont;
     // Start is called before the first frame update
     void Start()
     {
-        char_cont = this.GetComponent<CharacterController>(); 
+        char_cont = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            DiaRunner.GetComponent<Yarn.Unity.DialogueRunner>().StartDialogue("Start");
 
-           
+        if (inside_npc_radius && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("yo");
+            StartNpcDialogue(current_npc_startNode);
         }
 
     }
 
-
-    public void NPC_Visited()
-    {
-        visited = true; 
-    }
     private void FixedUpdate()
     {
         if (input.x != 0 && input.y != 0)
@@ -56,18 +52,40 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Character" || collision.tag == "PickUp")
+        Debug.Log("Colliding with npc");
+
+        if (collision.tag == "NPC" || collision.tag == "PickUp")
         {
-            interaction_bubble.SetActive(true);
+            //Debug.Log("Colliding with npc");
+            inside_npc_radius = true;
+            //interaction_bubble.SetActive(true);
+
+
+            Yarn.Unity.Example.NPC collided_npc = collision.gameObject.GetComponent<Yarn.Unity.Example.NPC>();
+            Debug.Log(collided_npc);
+            current_npc_startNode = collided_npc.talkToNode;
+            //collided_npc.GivePlayerItem();
+
         }
 
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.tag == "Character" || collision.tag == "PickUp")
+        if (collision.tag == "NPC" || collision.tag == "PickUp")
         {
-            interaction_bubble.SetActive(false);
+           // interaction_bubble.SetActive(false);
+            inside_npc_radius = false;
+            current_npc_startNode = "";
+        }
+    }
+
+    public void StartNpcDialogue(string startNode)
+    {
+        if (startNode != null)
+        {
+            // Kick off the dialogue at this node.
+            FindObjectOfType<DialogueRunner>().StartDialogue(startNode);
         }
     }
 }
